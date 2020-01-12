c4b0637cba5c1b1ff431ea017a1d46a3a049444a
PaulShestakov <paulshestakov@icloud.com>
2020-01-13 01:59:32 +0300

    Switch to dagred3 renderer completely

--- media/main.js
+++ media/main.js
@@ -1,7 +1,11 @@
-(function () {
-  const vscode = acquireVsCodeApi();
+(function() {
+  // const vscode = acquireVsCodeApi();
 
   console.log("JS code is included!");
+
+  function renderError() {
+    const error = "";
+  }
 
   window.addEventListener("message", event => {
     console.log("Message arrived");
@@ -12,41 +16,44 @@
       case "UPDATE":
         const g = new dagreD3.graphlib.json.read(JSON.parse(message.data));
 
-        console.log(message.data);
+        // console.log(message.data);
 
         var svg = d3.select("svg"),
           inner = svg.select("g");
         // Set up zoom support
-        var zoom = d3.zoom().on("zoom", function () {
+        var zoom = d3.zoom().on("zoom", function() {
           inner.attr("transform", d3.event.transform);
         });
         svg.call(zoom);
         // Create the renderer
         var render = new dagreD3.render();
         // Run the renderer. This is what draws the final graph.
-        render(inner, g);
-        // Center the graph
-        var initialScale = 1;
+        try {
+          g.graph().transition = function(selection) {
+            return selection.transition().duration(500);
+          };
 
-        // svg.attr("width", g.graph().width * initialScale + 40);
-        // svg.attr("height", g.graph().height * initialScale + 40);
+          render(inner, g);
 
-        var body = document.getElementById("body");
+          // Center the graph
+          var initialScale = 1;
 
-        const svgWidth = +svg.style("width").slice(0, -2);
-        const svgHeight = +svg.style("height").slice(0, -2);
+          const svgWidth = +svg.style("width").slice(0, -2);
+          const svgHeight = +svg.style("height").slice(0, -2);
 
-        svg.call(
-          zoom.transform,
-          d3.zoomIdentity
-            .translate(
-              (svgWidth - g.graph().width * initialScale) / 2,
-              (svgHeight - g.graph().height * initialScale) / 2
-            )
-            .scale(initialScale)
-        );
-        console.log(svg)
-        console.log(window)
+          svg.call(
+            zoom.transform,
+            d3.zoomIdentity
+              .translate(
+                (svgWidth - g.graph().width * initialScale) / 2,
+                (svgHeight - g.graph().height * initialScale) / 2
+              )
+              .scale(initialScale)
+          );
+        } catch (error) {
+          console.log(error);
+        }
+
         break;
     }
   });
