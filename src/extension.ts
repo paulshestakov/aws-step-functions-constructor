5756a2edab843b4c3c534677a111f1cf5872758b
PaulShestakov <paulshestakov@icloud.com>
2020-10-28 01:10:17 +0300

    Refactoring: extracted webview staff

--- src/extension.ts
+++ src/extension.ts
@@ -1,58 +1,5 @@
 import * as vscode from "vscode";
-import parse from "./openedFile";
-import { buildGraph } from "./buildGraph";
-import * as path from "path";
-import { getWebviewHtmlTemplate } from "./rendering/render";
-import { debounce } from "./utils/debounce";
-import { getStepFunctionViewName, getSourceMap } from "./openedFile/openedFile";
-import { getStates } from "./stepFunction";
-
-let URI;
-
-async function updateContent(panel, uri, fileName) {
-  try {
-    const stepFunction = await parse(uri, fileName);
-    const serializedGraph = buildGraph(stepFunction);
-    const states = getStates(stepFunction);
-
-    panel.webview.postMessage({
-      command: "UPDATE",
-      data: {
-        serializedGraph,
-        states
-      }
-    });
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-const updateContentDebounced: any = debounce(updateContent, 300);
-
-function createWebviewPanel(context: vscode.ExtensionContext) {
-  const stepFunctionViewName = getStepFunctionViewName();
-
-  const resourceColumn =
-    (vscode.window.activeTextEditor &&
-      vscode.window.activeTextEditor.viewColumn) ||
-    vscode.ViewColumn.One;
-
-  return vscode.window.createWebviewPanel(
-    "stepFunction.constructor",
-    stepFunctionViewName,
-    resourceColumn + 1,
-    {
-      enableScripts: true,
-      localResourceRoots: [
-        vscode.Uri.file(path.join(context.extensionPath, "media"))
-      ]
-    }
-  );
-}
-
-function setWebviewHtmlTemplate(panel, context: vscode.ExtensionContext) {
-  panel.webview.html = getWebviewHtmlTemplate(context.extensionPath);
-}
+import { createWebviewPanel, renderTemplate, postData, throttledPostData, makeHandleReceiveMessage } from "./webView";
 
 export function activate(context: vscode.ExtensionContext) {
   let disposable = vscode.commands.registerCommand(
@@ -63,65 +10,20 @@ export function activate(context: vscode.ExtensionContext) {
 
       const panel = createWebviewPanel(context);
 
-      setWebviewHtmlTemplate(panel, context);
+      renderTemplate(panel, context);
 
-      updateContent(panel, uri, fileName);
+      postData(panel, uri, fileName);
 
       vscode.workspace.onDidChangeTextDocument(async event => {
         const isActiveDocumentEdit = event.document.uri.fsPath === uri.fsPath;
         const hasSomethingChanged = event.contentChanges.length > 0;
 
         if (isActiveDocumentEdit && hasSomethingChanged) {
-          updateContentDebounced(panel, uri, fileName);
+          throttledPostData(panel, uri, fileName);
         }
       }, null);
 
-      panel.webview.onDidReceiveMessage(async message => {
-        switch (message.command) {
-          case "STATE_UPDATE":
-            console.log(JSON.stringify(message.data));
-            const sourceMap = await getSourceMap(uri);
-
-            const pointer = Object.keys(sourceMap.pointers).find(key => {
-              return key.endsWith(
-                `${message.data.stateName}/${message.data.statePropertyName}`
-              );
-            });
-            if (!pointer) {
-              return;
-            }
-
-            const pointerMap = sourceMap.pointers[pointer];
-
-            vscode.workspace.openTextDocument(uri).then(document => {
-              const edit = new vscode.WorkspaceEdit();
-
-              var textRange = new vscode.Range(
-                pointerMap.value.line,
-                pointerMap.value.column + 1,
-                pointerMap.valueEnd.line,
-                pointerMap.valueEnd.column - 1
-              );
-
-              edit.replace(
-                document.uri,
-                textRange,
-                message.data.statePropertyValue
-              );
-
-              vscode.workspace.applyEdit(edit).then(
-                editsApplied => {
-                  console.log("Applied");
-                },
-                reason => {
-                  console.warn(reason);
-                  vscode.window.showErrorMessage(`Error applying edits`);
-                }
-              );
-            });
-            return;
-        }
-      }, null);
+      panel.webview.onDidReceiveMessage(makeHandleReceiveMessage(uri), null);
     }
   );
 
---
+++ src/webView.ts
@@ -0,0 +1,98 @@
+import * as vscode from "vscode";
+import * as path from "path";
+import * as _ from "lodash";
+
+import parse from "./openedFile";
+import { buildGraph } from "./buildGraph";
+import { debounce } from "./utils/debounce";
+import { getStates } from "./stepFunction";
+import { getWebviewHtmlTemplate } from "./rendering/render";
+import { getStepFunctionViewName, getSourceMap } from "./openedFile/openedFile";
+
+export const createWebviewPanel = (context: vscode.ExtensionContext) => {
+  const stepFunctionViewName = getStepFunctionViewName();
+
+  const resourceColumn =
+    (vscode.window.activeTextEditor &&
+      vscode.window.activeTextEditor.viewColumn) ||
+    vscode.ViewColumn.One;
+
+  return vscode.window.createWebviewPanel(
+    "stepFunction.constructor",
+    stepFunctionViewName,
+    resourceColumn + 1,
+    {
+      enableScripts: true,
+      localResourceRoots: [
+        vscode.Uri.file(path.join(context.extensionPath, "media"))
+      ]
+    }
+  );
+}
+
+export const renderTemplate = (panel, context: vscode.ExtensionContext) => {
+  panel.webview.html = getWebviewHtmlTemplate(context.extensionPath);
+}
+
+export const postData = async (panel, uri, fileName) => {
+  try {
+    const stepFunction = await parse(uri, fileName);
+    const serializedGraph = buildGraph(stepFunction);
+    const states = getStates(stepFunction);
+
+    panel.webview.postMessage({
+      command: "UPDATE",
+      data: {
+        serializedGraph,
+        states
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+export const throttledPostData: any = _.throttle(postData, 200);
+
+export const makeHandleReceiveMessage = (uri: vscode.Uri) => async message => {
+  switch (message.command) {
+    case "STATE_UPDATE":
+      const sourceMap = await getSourceMap(uri);
+
+      const pointer = Object.keys(sourceMap.pointers).find(key => {
+        return key.endsWith(
+          `${message.data.stateName}/${message.data.statePropertyName}`
+        );
+      });
+      if (!pointer) {
+        return;
+      }
+
+      const pointerMap = sourceMap.pointers[pointer];
+
+      vscode.workspace.openTextDocument(uri).then(document => {
+        const edit = new vscode.WorkspaceEdit();
+
+        var textRange = new vscode.Range(
+          pointerMap.value.line,
+          pointerMap.value.column + 1,
+          pointerMap.valueEnd.line,
+          pointerMap.valueEnd.column - 1
+        );
+
+        edit.replace(
+          document.uri,
+          textRange,
+          message.data.statePropertyValue
+        );
+
+        vscode.workspace.applyEdit(edit).then(
+          editsApplied => {
+          },
+          reason => {
+            vscode.window.showErrorMessage(`Error applying edits`);
+          }
+        );
+      });
+      return;
+  }
+}
