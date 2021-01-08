5f0d72ac935963891eaa5a4df1f8b15e51872fd2
PaulShestakov <paulshestakov@icloud.com>
2021-01-08 22:29:17 +0300

    Show default choice label

--- src/buildGraph.ts
+++ src/buildGraph.ts
@@ -1,5 +1,6 @@
 import * as dagreD3 from "dagre-d3";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
+import * as R from "ramda";
 
 import { StepFunction, Operator, stringifyChoiceOperator } from "./stepFunction";
 
@@ -10,13 +11,11 @@ function makeGroupName() {
 }
 
 export function buildGraph(stepFunction: StepFunction) {
-  var g = new graphlib.Graph({ compound: true })
-    .setGraph({})
-    .setDefaultEdgeLabel(function () {
-      return {};
-    });
+  var g = new graphlib.Graph({ compound: true }).setGraph({}).setDefaultEdgeLabel(function () {
+    return {};
+  });
 
-  function traverse(stepFunction: StepFunction, g, groupName?) {
+  function traverse(stepFunction: StepFunction, g: dagreD3.graphlib.Graph, groupName?: string) {
     const startAtName = stepFunction.StartAt;
 
     if (groupName) {
@@ -25,9 +24,7 @@ export function buildGraph(stepFunction: StepFunction) {
 
     let statesToAddToParent = new Set(Object.keys(stepFunction.States));
 
-    Object.keys(stepFunction.States).forEach(stateName => {
-      const state = stepFunction.States[stateName];
-
+    R.toPairs(stepFunction.States).forEach(([stateName, state]) => {
       if (stateName === startAtName && !groupName) {
         g.setNode(stateName, { label: stateName, style: "fill: #fcba03;" });
       } else if (state.End && !groupName) {
@@ -38,52 +35,52 @@ export function buildGraph(stepFunction: StepFunction) {
 
       switch (state.Type) {
         case "Parallel": {
-          const groupName = makeGroupName();
-          g.setNode(groupName, {
+          const newGroupName = makeGroupName();
+          g.setNode(newGroupName, {
             label: "Parallel",
             style: "stroke: #000; stroke-width: 3px; stroke-dasharray: 5, 5;",
-            clusterLabelPos: "top"
+            clusterLabelPos: "top",
           });
 
-          state.Branches.forEach(branch => {
+          state.Branches.forEach((branch) => {
             g.setEdge(stateName, branch.StartAt, { label: "" });
 
-            traverse(branch, g, groupName);
+            traverse(branch, g, newGroupName);
 
-            Object.keys(branch.States).forEach(stateName => {
-              const branchEndState = branch.States[stateName];
-              if (branchEndState.End) {
-                g.setEdge(stateName, state.Next);
-              }
-            });
+            R.toPairs(branch.States)
+              .filter(([branchStateName, branchState]) => Boolean(branchState.End))
+              .forEach(([branchStateName, branchState]) => g.setEdge(branchStateName, state.Next));
           });
           break;
         }
         case "Choice": {
           if (state.Choices) {
-            const chioceGroupName = makeGroupName();
-            g.setNode(chioceGroupName, {
+            const newGroupName = makeGroupName();
+            g.setNode(newGroupName, {
               label: "Choice",
               style: "fill: #d9dddc",
-              clusterLabelPos: "top"
+              clusterLabelPos: "top",
             });
 
             if (groupName) {
-              g.setParent(chioceGroupName, groupName);
+              g.setParent(newGroupName, groupName);
             }
 
             state.Choices.forEach((choice: Operator) => {
               g.setEdge(stateName, choice.Next, {
                 label: stringifyChoiceOperator(choice),
-                labelStyle: "font-style: italic;"
+                labelStyle: "font-style: italic;",
               });
-              g.setParent(choice.Next, chioceGroupName);
+              g.setParent(choice.Next, newGroupName);
               statesToAddToParent.delete(choice.Next);
             });
 
             if (state.Default) {
-              g.setEdge(stateName, state.Default);
-              g.setParent(state.Default, chioceGroupName);
+              g.setEdge(stateName, state.Default, {
+                label: "Default",
+                labelStyle: "font-style: italic;",
+              });
+              g.setParent(state.Default, newGroupName);
               statesToAddToParent.delete(state.Default);
             }
           }
@@ -97,14 +94,13 @@ export function buildGraph(stepFunction: StepFunction) {
     });
 
     if (groupName) {
-      [...statesToAddToParent].forEach(stateName => {
+      [...statesToAddToParent].forEach((stateName) => {
         g.setParent(stateName, groupName);
       });
     }
   }
 
   traverse(stepFunction, g);
-
   ensureUnspecifiedNodes(g);
   roundNodes(g);
 
@@ -112,7 +108,7 @@ export function buildGraph(stepFunction: StepFunction) {
 }
 
 function ensureUnspecifiedNodes(g) {
-  g.edges().forEach(edge => {
+  g.edges().forEach((edge) => {
     if (!g.node(edge.v)) {
       g.setNode(edge.v, { label: edge.v, style: "fill: #ff0000;" });
     }
